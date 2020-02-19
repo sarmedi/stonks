e68b30b6389f8744250c6bf5f81ca63f5d69076d
Sarmed Imadulla <sarmed.imadulla@gmail.com>
2020-02-18 19:27:46 -0500

    Update LinksScreen.js

--- Stonks/screens/LinksScreen.js
+++ Stonks/screens/LinksScreen.js
@@ -29,20 +29,6 @@ export default class App extends React.Component {
   }
 }
 
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
