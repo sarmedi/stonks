389880e32499619cb72a2ada2d38d859c3872976
Alex Ding <alexd3301@gmail.com>
2020-04-05 23:32:22 -0400

    tutorial2

--- Stonks/screens/TutorialScreen.js
+++ Stonks/screens/TutorialScreen.js
@@ -15,7 +15,46 @@ export default function TutorialScreen() {
 
             <Text style={styles.codeHighlightContainer}>
               {"Stock markets are where buyers and sellers of stocks come together to trade shares in companies."}
+            </Text>
+
+            <View style={styles.welcomeContainer}>
+              <Image
+                source={
+                  __DEV__
+                    ? require('../assets/images/tutorial1.png')
+                    : require('../assets/images/tutorial1.png')
+                }
+                style={styles.welcomeImage}
+              />
+            </View>
+
+            <Text style={styles.titleText2}>
+              {"Understanding the stock market"}
+            </Text>
+
+            <Text>
               {"\n"}
+              {"Stocks are bought and sold on stock markets, which bring together buyers and sellers of publicly traded company shares. Stock markets operate kind of like auctions, where potential buyers name their highest price (“the bid”) and potential sellers name their lowest price (“the ask”). The actual price a trade is executed at is somewhere between the bid and the ask. Trades can be placed by stockbrokers, usually on behalf of portfolio managers or individual investors like you. The stock market in the US is made up of 13 exchanges, the best known are the New York Stock Exchange and Nasdaq."}
+              {"\n"}
+            </Text>
+
+            <Text style={styles.codeHighlightContainer}>
+              {"Snapchat-owner Snap Inc. listed its shares publicly on the stock market with its 2016 IPO. Its shares now trade on the New York Stock Exchange with the ticker symbol “SNAP,” and they’re available to buy and sell on the stock market by everyday investors like you."}
+            </Text>
+
+            <Text style={styles.titleText2}>
+              {"\n"}
+              {"Takeaway"}
+              {"\n"}
+            </Text>
+
+            <Text style={styles.codeHighlightContainer}>
+              {"The world’s stock markets are complex, but are all based upon one simple concept..."} 
+            </Text>
+
+             <Text>
+              {"\n"}
+              {"Connecting stock buyers with stock sellers to trade under an agreed upon set of rules. This is the key role of every stock market, from New York to Hong Kong."}
               {"\n"}
             </Text>
 
@@ -23,22 +62,40 @@ export default function TutorialScreen() {
               <Image
                 source={
                   __DEV__
-                    ? require('../assets/images/app_icon.png')
-                    : require('../assets/images/app_icon.png')
+                    ? require('../assets/images/tutorial2.png')
+                    : require('../assets/images/tutorial2.png')
                 }
                 style={styles.welcomeImage}
               />
             </View>
 
+            <Text> {"\n"} </Text>
+
             <Text numberOfLines={5}>{"some more text"}</Text>
-          
+            
+            <Text> {"\n"} </Text>
+            
+            <Text> {"\n"} </Text>
+
+
         </ScrollView>
     );
   }
 
 const styles = StyleSheet.create({
+   welcomeImage: {
+    width: 500,
+    height: 150,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
   titleText: {
     fontSize: 21,
+    fontWeight: "bold"
+  },
+  titleText2: {
+    fontSize: 19,
     fontWeight: "bold"
   },
   container: {
@@ -72,5 +129,10 @@ const styles = StyleSheet.create({
     fontSize: 15,
     alignSelf: 'flex-start',
     marginTop: 1,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
   },
 });
